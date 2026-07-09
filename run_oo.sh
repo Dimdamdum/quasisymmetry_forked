ec0712a57017f2cc05b20baf2d476879859f1445
Alexey Uvarov <alexejuv@gmail.com>
2026-07-09 17:35:57 -0400

    add visualization stuff for jsons

--- run_oo.sh
+++ run_oo.sh
@@ -1,20 +1,23 @@
 #! /bin/bash
 
-h="hamiltonians/H4/H4_linear_d1.0890.chk"
+hamiltonians=( "hamiltonians/H4/H4_linear_d2.5560.chk" "hamiltonians/H4/H4_linear_d2.0670.chk" "hamiltonians/H4/H4_linear_d1.0890.chk")
 parity_matrices=( "hamiltonians/H4/parity_4_sens.txt" "hamiltonians/H4/LNE_2_out_of_8_so.txt" )
 ref_states=( "hf" "fci" )
 cost_functions=( "NC" "variance" "decoupled" "fixed_sector" "switching_sector" )
 # cost_functions=( "switching_sector" )
 
-
-for parity_sen in "${parity_matrices[@]}"
-do 
-  for ref in "${ref_states[@]}"
-  do
-    for cost in "${cost_functions[@]}"
-    do
-      echo "$parity_sen $ref $cost"
-      python optimize_symmetries.py $h $parity_sen --reference $ref --cost_function $cost
-    done
-  done
+for h in "${hamiltonians[@]}"
+do
+	for parity_sen in "${parity_matrices[@]}"
+	do 
+		for ref in "${ref_states[@]}"
+		do
+		  for cost in "${cost_functions[@]}"
+		  do
+		    echo "$parity_sen $ref $cost"
+		    #python optimize_symmetries.py $h $parity_sen --reference $ref --cost_function $cost
+		    python optimize_symmetries.py $h $parity_sen --reference $ref --cost_function $cost --optimizer_maxiter 0
+		  done
+		done
+	done
 done
